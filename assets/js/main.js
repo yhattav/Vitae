76792a87f9fcb39d3931270e4d33c878cb389ace
yhattav <yhattav@gmail.com>
2016-03-17 02:09:08 +0200

    commit after background progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
     triggerElement: '#trigger1'
-}).setTween('#animate1', 0.5, {backgroundColor: 'green', scale: 2.5})
+}).setTween('#animate1', 2, {backgroundColor: 'green', scale: 2.5})
 .addIndicators({name: "1-no duration"})
 .addTo(controller);
+document.('.mainpage').style.backgroundImage = "url('../images/Aurora-Ocean2.jpg')";
