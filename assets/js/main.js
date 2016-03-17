70aa1f088154545d0e4cd92544113db084c3464f
yhattav <yhattav@gmail.com>
2016-03-17 15:14:18 +0200

    before changeing to manipulate background

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
-    triggerElement: '#trigger1'
+    triggerElement: '#trigger1', duration: 300
 }).setTween('#animate1', 2, {backgroundColor: 'green', scale: 2.5})
 .addIndicators({name: "1-no duration"})
 .addTo(controller);
-document.('.mainpage').style.backgroundImage = "url('../images/Aurora-Ocean2.jpg')";
