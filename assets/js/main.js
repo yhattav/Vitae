ffa212080391b121ff64d0735df82eafe858975b
yhattav <yhattav@gmail.com>
2016-03-17 17:55:44 +0200

    working backgrond

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
-    triggerElement: '#trigger1', duration: 300
-}).setTween('#animate1', 2, {backgroundColor: 'green', scale: 2.5})
+    duration: 3000
+}).setTween('#animate1', {scale: 1.03}).setPin('#animate1')
 .addIndicators({name: "1-no duration"})
+.addTo(controller);
+var controller = new ScrollMagic.Controller();
+var scene1 = new ScrollMagic.Scene({
+    duration: 3000
+}).setTween('#animate2', {scale: 1.06, top: 400, left: 300}).setPin('#animate2')
+.addIndicators({name: "2-no "})
 .addTo(controller);
