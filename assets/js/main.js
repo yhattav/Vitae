c09825fce47fc70702326262ff421aa103acb6cb
yhattav <yhattav@gmail.com>
2016-03-18 07:48:47 +0200

    Before Class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
     duration: 3000
-}).setTween('#animate1', {scale: 1.03}).setPin('#animate1')
+}).setTween('#animate1', {scale: 1.00, top: -100}).setPin('#animate1')
 .addIndicators({name: "1-no duration"})
 .addTo(controller);
 var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({
     duration: 3000
-}).setTween('#animate2', {scale: 1.06, top: 400, left: 300}).setPin('#animate2')
+}).setTween('#animate2', {scale: 1.00, top: -2000, left: 200}).setPin('#animate2')
 .addIndicators({name: "2-no "})
 .addTo(controller);
